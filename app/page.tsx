c21d148e39493a90160a43ce31a83afc0a1a22d0
nvillamizar-dev <nixonbusiness12@gmail.com>
2026-05-26 18:54:49 -0500

    feat(homepage): add TrendingCoin type definition and table columns with name, logo and 24h price change

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,49 @@
 import React from "react";
 import Image from "next/image";
+import DataTable from "@/components/DataTable";
+import { Link } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { TrendingUp, TrendingDown } from "lucide-react";
+
+const columns: DataTableColumn<TrendingCoin>[] = [
+  {
+    header: 'Nombre',
+    cellClassName: 'name-cell',
+    cell: (coin) => {
+      const item = coin.item;
+      return (
+        <Link href={`/coins/${item.id}`} >
+          <Image src={item.large} alt={item.name} width={36} height={36} />
+          <p>{item.name}</p>
+        </Link>
+
+      )
+    },
+  },
+  {
+    header: 'Cambio en 24',
+    cellClassName: 'name-cell',
+    cell: (coin) => {
+      const item = coin.item;
+      const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
+
+      return (
+        <div className={cn('price-change', isTrendingUp ? 'text-green-500' : 'text-red-500')}>
+          <p>
+            {isTrendingUp ? (
+              <TrendingUp width={16} height={16} />
+            ) :
+
+              <TrendingDown width={16} height={16} />
+            }
+          </p>
+
+        </div>
+      )
+    }
+  }
+
+]
 
 const Page = () => {
   return <main className="main-container">
@@ -14,6 +58,9 @@ const Page = () => {
         </div>
       </div>
       <p>Monedas en tendencia</p>
+      <DataTable
+        data={[]}
+        columns={[{ header: 'Titulo' }, { header: 'Precio' }]} />
     </section>
 
     <section className="w-full mt-7 space-y-4">
